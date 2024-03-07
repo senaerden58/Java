2f89d52283b6e8c94eb8633774e2d3b8c31920bd
senaerden58 <132837240+senaerden58@users.noreply.github.com>
2024-03-07 20:46:14 +0300

    Hipotenüsü bulan program

---
+++ src/Pisagor/Pisagor.java
@@ -0,0 +1,20 @@
+package Pisagor;
+import java.util.Scanner;
+
+public class Pisagor {
+	public static void main(String[] args)
+	{
+		int a,b;
+		double c;
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter the 1st edge: ");
+		a=input.nextInt();
+		System.out.print("Enter the 2nd edge: ");
+		b=input.nextInt();
+		
+		c=Math.sqrt((a*a)+(b*b));
+		
+		System.out.print("3rd edge: " + c);
+		
+	}
+}
